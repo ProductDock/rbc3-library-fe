e7bc63e8dc72827bab7c453e4290f0beaa59b560
Dunja Gojković <gojkovic.ra37.2018@uns.ac.rs>
2024-09-27 15:58:46 +0200

    added style from figma design for text on login page

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -12,9 +12,9 @@ export default function LoginPage() {
         </div>
       </div>
       <div className='content-holder'>
-        <h2>Hello, welcome to...</h2>
-        <h1>ProductDock Library.</h1>
-        <p>
+        <h2 className='hello-text'>Hello, welcome to...</h2>
+        <h1 className='library-text'>ProductDock Library</h1>
+        <p className='long-text'>
           Looking for the next book to read? Explore our collection and discover
           your new favorite that perfectly matches your interests.
         </p>
