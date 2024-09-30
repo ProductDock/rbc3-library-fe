61c538a26a7d047f7e7fa2317234b9839c836b1f
Aleksandra-Romanic <aleksandra.romanic@productdock.com>
2024-09-30 11:21:52 +0200

    Added LetsStartExploring with module.css

---
+++ src/pages/Homepage/LetsStartExploring.tsx
@@ -0,0 +1,15 @@
+import { Box, Stack } from '@mui/material'
+import styles from './LetsStartExploring.module.css'
+
+export const LetsStartExploring = () => {
+  return (
+    <Stack
+      direction='row'
+      height='1500px'
+      className={styles.LetsStartExploring}
+    >
+      <h1>Lets start exploring</h1>
+      <h2>.</h2>
+    </Stack>
+  )
+}
