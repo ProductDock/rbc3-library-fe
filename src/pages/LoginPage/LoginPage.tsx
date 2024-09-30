c196bce5dc25b81183a067b6337e1d73e501aa29
Dunja Gojković <gojkovic.ra37.2018@uns.ac.rs>
2024-09-30 13:29:08 +0200

    updated image holder div, using flex instead of grid

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -5,16 +5,14 @@ import './LoginPage.css'
 export default function LoginPage() {
   return (
     <div className='wrap'>
-      <div className='img-holder'>
-        <img src={CircleLogo} className='img-logo' />
-        <div className='img-container'>
-          <img src={LoginPagePhoto} alt='photo' className='image' />
-        </div>
+      <div className='imgHolder'>
+        <img src={CircleLogo} className='imgLogo' />
+        <img src={LoginPagePhoto} alt='photo' className='img' />
       </div>
-      <div className='content-holder'>
-        <h2 className='hello-text'>Hello, welcome to...</h2>
-        <h1 className='library-text'>ProductDock Library</h1>
-        <p className='long-text'>
+      <div className='contentHolder'>
+        <h2 className='helloText'>Hello, welcome to...</h2>
+        <h1 className='libraryText'>ProductDock Library.</h1>
+        <p className='longText'>
           Looking for the next book to read? Explore our collection and discover
           your new favorite that perfectly matches your interests.
         </p>
