091d084b42c11e7bbd57e15dc0e35d4e9316d670
Isidora Brasancevic <isidora.brasancevic@productdock.com>
2024-10-03 16:09:57 +0200

    Update style and map categories

--- src/pages/Homepage/BookCatalogueSection/BookCatalogueSection.tsx
+++ src/pages/Homepage/BookCatalogueSection/BookCatalogueSection.tsx
@@ -2,8 +2,32 @@ import Typography from '@mui/material/Typography'
 import styles from './BookCatalogueSection.module.css'
 import Button from '@mui/material/Button'
 import Divider from '@mui/material/Divider'
+import FormControl from '@mui/material/FormControl'
+import Select, { SelectChangeEvent } from '@mui/material/Select'
+import MenuItem from '@mui/material/MenuItem'
+import { useState } from 'react'
 
 export const BookCatalogueSection = () => {
+  const [status, setStatus] = useState('')
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setStatus(event.target.value)
+  }
+
+  const [activeButton, setActiveButton] = useState(0)
+
+  const categories = [
+    'All',
+    'Software development',
+    'Marketing',
+    'Product management',
+    'Design',
+    'Psychology',
+  ]
+
+  const handleButtonClick = (index: number) => {
+    setActiveButton(index)
+  }
   return (
     <div className={styles.catalogueWrapper}>
       <div className={styles.titleButtonWrapper}>
@@ -14,17 +38,71 @@ export const BookCatalogueSection = () => {
       </div>
       <Divider />
       <div className={styles.categoryStatusWrapper}>
+        {/* <div className={styles.categoriesWrapper}>
+          <Button className={styles.category}>All</Button>
+          <Button className={styles.category}>Software development</Button>
+          <Button className={styles.category}>Marketing</Button>
+          <Button className={styles.category}>Product management</Button>
+          <Button className={styles.category}>Design</Button>
+          <Button className={styles.category}>Psychology</Button>
+        </div> */}
         <div className={styles.categoriesWrapper}>
-          <div className={styles.category}>All</div>
-          <div className={styles.category}>Software development</div>
-          <div className={styles.category}>Marketing</div>
-          <div className={styles.category}>Product management</div>
-          <div className={styles.category}>Design</div>
-          <div className={styles.category}>Psychology</div>
+          {categories.map((label, index) => (
+            <Button
+              key={index}
+              className={`${styles.category} ${
+                activeButton === index ? styles.clicked : ''
+              }`}
+              onClick={() => handleButtonClick(index)}
+            >
+              {label}
+            </Button>
+          ))}
         </div>
         <div className={styles.sortByWrapper}>
           <Typography className={styles.sortBy}>Sort by:</Typography>
-          <div>All statuses</div>
+          <div>
+            <FormControl
+              variant='standard'
+              sx={{ color: 'rgba(0, 0, 0)', fill: 'rgba(0, 0, 0)' }}
+            >
+              <Select
+                color={undefined}
+                sx={{
+                  color: 'black',
+                  // '& .MuiOutlinedInput-notchedOutline': {
+                  //   borderColor: 'black',
+                  // },
+                  '& .MuiSvgIcon-root': {
+                    color: 'black',
+                  },
+                }}
+                className={styles.statusForm}
+                disableUnderline={true}
+                value={status}
+                onChange={handleChange}
+                displayEmpty
+                inputProps={{ 'aria-label': 'Without label' }}
+              >
+                <MenuItem value=''>
+                  <em
+                    className={`${styles.allStatusesMenuItem} ${styles.menuItem}`}
+                  >
+                    All statuses
+                  </em>
+                </MenuItem>
+                <MenuItem className={styles.menuItem} value='Available'>
+                  Available books
+                </MenuItem>
+                <MenuItem className={styles.menuItem} value='Reserved'>
+                  Reserved books
+                </MenuItem>
+                <MenuItem className={styles.menuItem} value='Rented'>
+                  Rented books
+                </MenuItem>
+              </Select>
+            </FormControl>
+          </div>
         </div>
       </div>
     </div>
