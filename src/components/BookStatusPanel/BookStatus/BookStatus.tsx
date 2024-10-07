53dee36a4adca6726dc559186ebd8bc6d8808b7b
Aleksandra-Romanic <aleksandra.romanic@productdock.com>
2024-10-07 14:26:22 +0200

    Added class names to the project

--- src/components/BookStatusPanel/BookStatus/BookStatus.tsx
+++ src/components/BookStatusPanel/BookStatus/BookStatus.tsx
@@ -1,24 +1,25 @@
 import { Status } from './Status'
 import styles from './BookStatus.module.css'
+import cx from 'classnames'
 
 interface BookProps {
   status: Status
 }
 
 const BookStatusRecord: Record<Status, string> = {
-  [Status.AVAILABLE]: styles.green,
-  [Status.RESERVED]: styles.yellow,
-  [Status.RESERVED_BY_ME]: styles.yellow,
-  [Status.RESERVED_BY]: styles.yellow,
-  [Status.RENTED]: styles.red,
-  [Status.RENTED_BY_ME]: styles.red,
-  [Status.RENTED_BY]: styles.red,
-  [Status.ON_WAIT_LIST]: styles.grey,
-  [Status.PENDING]: styles.grey,
-  [Status.APPROVED]: styles.green,
-  [Status.DENIED]: styles.red,
-  [Status.ORDERED]: styles.yellow,
-  [Status.IN_LIBRARY]: styles.green,
+  [Status.AVAILABLE]: cx(styles.status, styles.green),
+  [Status.RESERVED]: cx(styles.status, styles.yellow),
+  [Status.RESERVED_BY_ME]: cx(styles.status, styles.yellow),
+  [Status.RESERVED_BY]: cx(styles.status, styles.yellow),
+  [Status.RENTED]: cx(styles.status, styles.red),
+  [Status.RENTED_BY_ME]: cx(styles.status, styles.red),
+  [Status.RENTED_BY]: cx(styles.status, styles.red),
+  [Status.ON_WAIT_LIST]: cx(styles.status, styles.grey),
+  [Status.PENDING]: cx(styles.status, styles.grey),
+  [Status.APPROVED]: cx(styles.status, styles.green),
+  [Status.DENIED]: cx(styles.status, styles.red),
+  [Status.ORDERED]: cx(styles.status, styles.yellow),
+  [Status.IN_LIBRARY]: cx(styles.status, styles.green),
 }
 
 const BookStatus = ({ status }: BookProps) => {
