9e0e95be93e2c2d68f4faa9f664d418e44b6d767
marko-gligic <marko.gligic@productdock.com>
2024-10-09 11:17:38 +0200

    Added all colors from the initial design

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,14 @@ import { Route, Routes } from 'react-router-dom'
 import { Homepage } from './pages/Homepage'
 import { LoginPage } from './pages/LoginPage'
 import { AuthorisedLayout } from './pages/AuthorisedLayout'
-import { ThemeProvider } from '@mui/material/styles';
-import { Theme } from './theme';
-import { CssBaseline } from '@mui/material';
-
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme/theme'
+import { CssBaseline } from '@mui/material'
 
 function App() {
   return (
     <>
-      <ThemeProvider theme={Theme}>
+      <ThemeProvider theme={theme}>
         <CssBaseline />
         <Routes>
           <Route path='/' element={<AuthorisedLayout />}>
