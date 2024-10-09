4b5ca10bf8a621b1ca69552c241612e995ab0a25
marko-gligic <marko.gligic@productdock.com>
2024-10-09 09:48:09 +0200

    Refactored gap between cards

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -14,12 +14,8 @@ const BookCard: React.FC<BookCardProps> = ({ inFavorites }) => {
       <Favorite inFavorites={inFavorites} />
       <div className={styles.cardContent}>
         <div className={styles.info}>
-          <div className={styles.author}>
-            <p className={styles.authorName}>Author placeholder</p>
-          </div>
-          <div className={styles.bookTitle}>
-            <h2 className={styles.title}>Book title placeholder</h2>
-          </div>
+          <div className={styles.author}>Author placeholder</div>
+          <div className={styles.bookTitle}>Book title placeholder</div>
           <Divider className={styles.divider} />
           <div className={styles.bookStatusPanel}>
             <BookStatusPanel />
