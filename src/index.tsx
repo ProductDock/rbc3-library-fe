0d550e8b287e3a4e3c536ebb9e87f4351b6cff02
Aleksandra-Romanic <aleksandra.romanic@productdock.com>
2024-10-09 09:50:10 +0200

    Added themeProvider and typography.ts to the project

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,17 @@ import './index.css'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
 import { StyledEngineProvider } from '@mui/material'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './theme/theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <StyledEngineProvider injectFirst>
       <BrowserRouter>
-        <App />
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
       </BrowserRouter>
     </StyledEngineProvider>
   </React.StrictMode>
---
+++ src/theme/typography.ts
@@ -0,0 +1,81 @@
+import { TypographyOptions } from '@mui/material/styles/createTypography'
+
+const typography: TypographyOptions = {
+  fontFamily: 'Poppins',
+  h1: {
+    fontWeight: 600,
+    fontSize: '96px',
+    lineHeight: '108px',
+    letterSpacing: '-4%',
+    '@media (max-width: 780px)': {
+      fontSize: '48px',
+      lineHeight: '64px',
+    },
+  },
+  h2: {
+    fontSize: 'clamp(24px, 5vw + 10px, 80px)',
+    fontWeight: 600,
+    lineHeight: '135%',
+    letterSpacing: '-4%',
+    '@media (max-width:1100px)': {
+      fontSize: '48px',
+      lineHeight: '64px',
+    },
+  },
+  h3: {
+    fontSize: '64px',
+    fontWeight: 600,
+    lineHeight: '80px',
+    letterSpacing: '-4%',
+  },
+  subtitle1: {
+    fontSize: '48px',
+    fontWeight: 600,
+    lineHeight: '64px',
+    letterSpacing: '-4%',
+  },
+  subtitle2: {
+    fontSize: '36px',
+    fontWeight: 600,
+    lineHeight: '48px',
+    letterSpacing: '-4%',
+  },
+  h4: {
+    fontSize: '24px',
+    fontWeight: 400,
+    lineHeight: '36px',
+    letterSpacing: '-3%',
+  },
+  h5: {
+    fontSize: '20px',
+    fontWeight: 400,
+    lineHeight: '32px',
+    letterSpacing: '-3%',
+    '@media (max-width:1100px)': {
+      fontSize: '16px',
+    },
+  },
+  h6: {
+    fontSize: '16px',
+    fontWeight: 400,
+    lineHeight: '28px',
+    letterSpacing: '-3%',
+    '@media (max-width:1100px)': {
+      fontSize: '14px',
+    },
+  },
+  body1: {
+    fontSize: '14px',
+    fontWeight: 400,
+    lineHeight: '24px',
+    letterSpacing: '-3%',
+  },
+  body2: {
+    fontSize: '12px',
+    fontWeight: 400,
+    lineHeight: '20px',
+    letterSpacing: '-3%',
+  },
+}
+
+export default typography
