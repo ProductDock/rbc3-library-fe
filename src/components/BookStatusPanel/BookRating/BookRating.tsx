94aad311b91a619d6b32eba69bdf9267d10366f6
Aleksandra-Romanic <aleksandra.romanic@productdock.com>
2024-10-10 09:05:58 +0200

    Implemented typography theme in BookStatusPanel

--- src/components/BookStatusPanel/BookRating/BookRating.tsx
+++ src/components/BookStatusPanel/BookRating/BookRating.tsx
@@ -1,11 +1,14 @@
 import styles from './BookRating.module.css'
 import star from '../../../assets/star.svg'
+import { Typography } from '@mui/material'
 
 export const BookRating = ({ rating }: { rating: number }) => {
   return (
     <div className={styles.ratingWrapper}>
       <img src={star} alt='rating-icon' className={styles.star} />
-      <span className={styles.rating}>{rating.toString()}</span>
+      <Typography variant='h6' className={styles.rating}>
+        {rating.toString()}
+      </Typography>
     </div>
   )
 }
