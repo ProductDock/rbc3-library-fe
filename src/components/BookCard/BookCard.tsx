6a88f69d022ed6d1d9f08404fe1d5de130ec7962
Aleksandra-Romanic <aleksandra.romanic@productdock.com>
2024-10-10 09:18:19 +0200

    Added theme for typography in BookCard

--- src/components/BookCard/BookCard.tsx
+++ src/components/BookCard/BookCard.tsx
@@ -3,6 +3,7 @@ import styles from './BookCard.module.css'
 import { Favorite } from './Favorite'
 import Divider from '@mui/material/Divider'
 import { BookStatusPanel } from '../BookStatusPanel'
+import { Typography } from '@mui/material'
 
 type BookCardProps = {
   inFavorites: boolean
@@ -14,8 +15,12 @@ const BookCard: React.FC<BookCardProps> = ({ inFavorites }) => {
       <Favorite inFavorites={inFavorites} />
       <div className={styles.cardContent}>
         <div className={styles.info}>
-          <div className={styles.author}>Author placeholder</div>
-          <div className={styles.bookTitle}>Book title placeholder</div>
+          <Typography variant='body1' className={styles.author}>
+            Author placeholder
+          </Typography>
+          <Typography variant='h5' className={styles.bookTitle}>
+            Book title placeholder
+          </Typography>
           <Divider className={styles.divider} />
           <div className={styles.bookStatusPanel}>
             <BookStatusPanel />
