6d100b10d9cff478b038e598c4d29a605c38c9aa
Aleksandra-Romanic <aleksandra.romanic@productdock.com>
2024-10-15 11:08:15 +0200

    Updated BookCard component for office manager view

--- src/components/BookCard/Favorite/Favorite.tsx
+++ src/components/BookCard/Favorite/Favorite.tsx
@@ -6,21 +6,24 @@ import styles from '../Favorite/Favorite.module.css'
 
 type FavoriteProps = {
   inFavorites: boolean
+  isAdmin?: boolean
 }
 
-const Favorite: React.FC<FavoriteProps> = ({ inFavorites }) => {
+const Favorite: React.FC<FavoriteProps> = ({ inFavorites, isAdmin }) => {
   return (
     <div className={styles.cardVisual}>
       <div className={styles.bookImage}>
         <img className={styles.image} src={book} alt='book' />
       </div>
-      <div className={styles.favorite}>
-        <img
-          className={styles.favoriteButton}
-          src={inFavorites ? favoriteFilled : favorite}
-          alt={inFavorites ? 'In favorites' : 'Not in favorites'}
-        />
-      </div>
+      {!isAdmin && (
+        <div className={styles.favorite}>
+          <img
+            className={styles.favoriteButton}
+            src={inFavorites ? favoriteFilled : favorite}
+            alt={inFavorites ? 'In favorites' : 'Not in favorites'}
+          />
+        </div>
+      )}
     </div>
   )
 }
