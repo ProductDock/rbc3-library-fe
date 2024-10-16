e2d88303e68ae17bcf5db13c3527321000566037
marko-gligic <marko.gligic@productdock.com>
2024-10-16 08:59:48 +0200

    Removed double header component from the office manager homepage

--- src/pages/ManagerHomepage/ManagerHomepage.tsx
+++ src/pages/ManagerHomepage/ManagerHomepage.tsx
@@ -1,11 +1,9 @@
-import { Header } from '../../components/Header'
 import { BookCatalogueSection } from '../Homepage/BookCatalogueSection'
 import { LetsStartExploring } from '../Homepage/LetsStartExploring'
 
 const Library = () => {
   return (
     <div>
-      <Header />
       <LetsStartExploring />
       <BookCatalogueSection isAdmin={true} />
     </div>
