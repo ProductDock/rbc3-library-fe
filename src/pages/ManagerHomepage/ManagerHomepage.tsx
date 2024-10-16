ccbd39335125d9998744c88b32f3ac01425bc511
Aleksandra-Romanic <aleksandra.romanic@productdock.com>
2024-10-16 12:41:46 +0200

    Admin view fix

--- src/pages/ManagerHomepage/ManagerHomepage.tsx
+++ src/pages/ManagerHomepage/ManagerHomepage.tsx
@@ -1,11 +1,18 @@
 import { BookCatalogueSection } from '../Homepage/BookCatalogueSection'
 import { LetsStartExploring } from '../Homepage/LetsStartExploring'
+import styles from '../Homepage/Homepage.module.css'
 
 const Library = () => {
   return (
-    <div>
-      <LetsStartExploring />
-      <BookCatalogueSection isAdmin={true} />
+    <div className={styles.wrapper}>
+      <div className={styles.catalogueWrapper}>
+        <div className={styles.exploringWrapper}>
+          <div>
+            <LetsStartExploring />
+          </div>
+        </div>
+        <BookCatalogueSection isAdmin={true} />
+      </div>
     </div>
   )
 }
