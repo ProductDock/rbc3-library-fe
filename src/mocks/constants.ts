86260ab038c11116bb547bdb953510912339a15c
aleksandar.stojanovic.ext <aleksandar.stojanovic.ext@adevinta.com>
2024-10-24 15:52:58 +0200

    chore: use prod URL

--- src/mocks/constants.ts
+++ src/mocks/constants.ts
@@ -1,6 +1,7 @@
 export const IS_PROD = import.meta.env.PROD
 export const BASE_URL_LOCAL = 'http://localhost:8080'
-export const BASE_URL_PROD = 'http://fake-prod:8080'
+export const BASE_URL_PROD =
+  'http://ecs-eu-west-1-nonprod-505016162.eu-west-1.elb.amazonaws.com:81'
 
 export const API_URL = IS_PROD ? BASE_URL_PROD : BASE_URL_LOCAL
 
