c213d08d028952359f89053ab25fbdd645fda63f
Aleksandra-Romanic <aleksandra.romanic@productdock.com>
2024-10-24 08:32:15 +0200

    RBN-91: Add write a review form (#32)
    * Added ReviewForm with appropriate module.css
    
    * Added StarRating component
    
    * Added RecomendationCheckBox
    
    * Fix in ReviewFrom
    
    * Added ReviewForm with appropriate module.css
    
    * Added StarRating component
    
    * Added RecomendationCheckBox
    
    * Fix in ReviewFrom
    
    * Implemented textField and styling inside ReviewForm
    
    * Changed style  of ReviewForm for mobile view
    
    * Added funcionality for select all in RecomendationCheckBox
    
    * Implemented Drawer in ReviewForm
    
    * Fix in RecomendationCheckBox and ReviewForm
    
    * Fixed ReviewForm module.css

---
+++ src/pages/ReviewForm/RecomendationCheckBox/RecomendationCheckBox.tsx
@@ -0,0 +1,78 @@
+import Checkbox from '@mui/material/Checkbox'
+import Typography from '@mui/material/Typography'
+import styles from '../ReviewForm.module.css'
+import CheckBoxOutlineBlankSharpIcon from '@mui/icons-material/CheckBoxOutlineBlankSharp'
+import CheckBoxSharpIcon from '@mui/icons-material/CheckBoxSharp'
+import { useEffect, useState } from 'react'
+import IndeterminateCheckBoxSharpIcon from '@mui/icons-material/IndeterminateCheckBoxSharp'
+
+const labels = [
+  { label: 'Select All', value: 'Select all', checked: false },
+  { label: 'Junior', value: 'Junior', checked: false },
+  { label: 'Medior', value: 'Medior', checked: false },
+  { label: 'Senior', value: 'Senior', checked: false },
+]
+
+export default function RecomendationCheckBox() {
+  const [checkBoxState, setCheckBoxState] = useState(labels)
+
+  useEffect(() => {
+    return () => {
+      const newCheckboxState = checkBoxState.map(el => {
+        el.checked = false
+        return el
+      })
+      setCheckBoxState(newCheckboxState)
+    }
+  }, [])
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    index: number
+  ) => {
+    const isChecked = event.target.checked
+    if (index === 0) {
+      const newCheckBoxState = checkBoxState.map(el => {
+        el.checked = isChecked
+        return el
+      })
+      setCheckBoxState([...newCheckBoxState])
+    } else {
+      const newCheckBoxState = [...checkBoxState]
+      const element = newCheckBoxState[index]
+      element.checked = isChecked
+      newCheckBoxState[0].checked = newCheckBoxState
+        .slice(1)
+        .every(el => el.checked)
+      setCheckBoxState([...newCheckBoxState])
+    }
+  }
+  return (
+    <div className={styles.checkBoxWrapper}>
+      {checkBoxState.map((item, index) => (
+        <div key={item.value} className={styles.checkBoxElements}>
+          <Checkbox
+            checked={item.checked}
+            indeterminate={
+              index == 0 &&
+              checkBoxState[index].checked === false &&
+              checkBoxState.slice(1).some(el => el.checked)
+            }
+            indeterminateIcon={
+              <IndeterminateCheckBoxSharpIcon
+                className={styles.customCheckBox}
+              />
+            }
+            size='small'
+            onChange={event => handleChange(event, index)}
+            icon={<CheckBoxOutlineBlankSharpIcon className={styles.checkBox} />}
+            checkedIcon={<CheckBoxSharpIcon className={styles.checkBox} />}
+          />
+          <Typography variant='h6' component='span'>
+            {item.label}
+          </Typography>
+        </div>
+      ))}
+    </div>
+  )
+}
---
+++ src/pages/ReviewForm/ReviewForm.tsx
@@ -0,0 +1,151 @@
+import {
+  Box,
+  Button,
+  Divider,
+  Drawer,
+  styled,
+  Typography,
+  useMediaQuery,
+} from '@mui/material'
+import styles from './ReviewForm.module.css'
+import { StarRating } from './StarRating'
+import { RecomendationCheckBox } from './RecomendationCheckBox'
+import TextField from '@mui/material/TextField'
+import close from '../../assets/closeBlack.svg'
+
+const StyledTextField = styled(TextField)(({ theme }) => ({
+  '& input::placeholder': {
+    fontSize: theme.typography.h6.fontSize,
+    lineHeight: theme.typography.h6.lineHeight,
+    letterSpacing: theme.typography.h6.letterSpacing,
+    opacity: 1,
+  },
+}))
+
+interface ReviewFormProps {
+  open: boolean
+  toggleDrawer: (newOpen: boolean) => void
+}
+
+export const ReviewForm: React.FC<ReviewFormProps> = ({
+  open,
+  toggleDrawer,
+}) => {
+  const matches = useMediaQuery('(min-width:700px)')
+
+  const formContent = (
+    <>
+      <div className={styles.reviewFormContainer}>
+        <div className={styles.formContainer}>
+          <div className={styles.closeBar} onClick={() => toggleDrawer(false)}>
+            <img src={close} alt='close' className={styles.closeIcon} />
+          </div>
+          <div className={styles.headerAndRatingContainer}>
+            <div className={styles.headerContainer}>
+              <Typography
+                variant={matches ? 'subtitle2' : 'h4'}
+                className={styles.headerText}
+              >
+                Write a review
+              </Typography>
+            </div>
+            <div className={styles.ratingContainer}>
+              <Typography variant='h6' className={styles.rateText}>
+                Rate
+                <span className={styles.star}>*</span>
+              </Typography>
+              <div>
+                <StarRating />
+              </div>
+            </div>
+          </div>
+          <Divider />
+          <div className={styles.recomendationContainer}>
+            <div>
+              <Typography
+                variant='h6'
+                className={styles.recomentationHeaderText}
+              >
+                Who would benefit from this book?
+              </Typography>
+            </div>
+            <div>
+              <RecomendationCheckBox />
+            </div>
+          </div>
+          <Divider />
+          <div className={styles.textFieldContainer}>
+            <div className={styles.reviewHeader}>
+              <Typography variant='h6' className={styles.rateText}>
+                Review
+                <span className={styles.star}>*</span>
+              </Typography>
+            </div>
+            <div className={styles.textAreaWrapper}>
+              <StyledTextField
+                variant='outlined'
+                multiline
+                rows={4}
+                className={styles.textArea}
+                placeholder='What is your personal opinion about this book'
+                slotProps={{
+                  input: {
+                    sx: {
+                      borderRadius: 0,
+                      '&:hover .MuiOutlinedInput-notchedOutline': {
+                        borderColor: 'var(--mui-palette-neutral-600)',
+                      },
+                      '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                        borderColor: 'var(--mui-palette-neutral-600)',
+                        opacity: 0.5,
+                      },
+                      '& .MuiOutlinedInput-input': {
+                        padding: '0px 16px',
+                      },
+                    },
+                  },
+                }}
+              />
+            </div>
+          </div>
+          <div className={styles.buttonContainer}>
+            <Button
+              className={styles.cancelButton}
+              onClick={() => toggleDrawer(false)}
+            >
+              <Typography variant='h6' className={styles.cancelButtonText}>
+                Cancel
+              </Typography>
+            </Button>
+            <Button className={styles.submitButton}>
+              <Typography variant='h6' className={styles.submitButtonText}>
+                Submit
+              </Typography>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+
+  return (
+    <Drawer
+      anchor='right'
+      open={open}
+      onClose={() => toggleDrawer(false)}
+      PaperProps={{
+        sx: {
+          width: '100%',
+          maxWidth: '800px',
+          height: '100%',
+        },
+      }}
+    >
+      <Box role='presentation' sx={{ overflowY: 'auto', height: '100%' }}>
+        {formContent}
+      </Box>
+    </Drawer>
+  )
+}
+
+export default ReviewForm
