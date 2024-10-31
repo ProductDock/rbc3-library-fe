2a8fc228a6d8cff48c18b19b1dae71d0ab213afc
Aleksandra-Romanic <aleksandra.romanic@productdock.com>
2024-10-31 16:17:18 +0100

    RBN-94: add more than one book UI (#37)
    * Enhanced component reusability, added accordion, and add new book button
    
    * implemented functionality for showing book cover in accordion and showing only one form at the time
    
    * Fixed typos and separated add another book button and submit button logic
    
    * Added RemoveBookButton
    
    * Fix in AddNewBooksForm
    
    * Refactored AddNewBooksForm

---
+++ src/pages/AddNewBooksForm/CustomComponents/RemoveBookButton.tsx
@@ -0,0 +1,37 @@
+import { Button, Typography } from '@mui/material'
+import style from '../AddNewBooksForm.module.css'
+import remove from '../../../assets/delete.svg'
+import { Book } from '../AddNewBooksForm'
+
+type RemoveBookProps = {
+  index?: number
+  addedBooks: Book[]
+  setAddedBooks?: (addedBooks: Book[]) => void
+}
+
+const RemoveBookButton = ({
+  index,
+  addedBooks,
+  setAddedBooks,
+}: RemoveBookProps) => {
+  const handleDelete = () => {
+    if (setAddedBooks !== undefined) {
+      console.log('index', index)
+
+      const newArr = addedBooks.filter(
+        (_, elementIndex) => elementIndex !== index
+      )
+      console.log('newArr', newArr)
+      setAddedBooks(newArr)
+    }
+  }
+  return (
+    <Button type='button' onClick={handleDelete}>
+      <img src={remove} alt='delete' />
+      <Typography variant='body1' className={style.removeBookButtonText}>
+        Remove this book
+      </Typography>
+    </Button>
+  )
+}
+export default RemoveBookButton
