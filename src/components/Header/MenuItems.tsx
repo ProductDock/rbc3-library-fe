40ccb4429c9364f9f9068e726c4a328828b8dbe0
Aleksandra-Romanic <aleksandra.romanic@productdock.com>
2024-12-23 15:55:56 +0100

    Updated SideBar and MenuItems

--- src/components/Header/MenuItems.tsx
+++ src/components/Header/MenuItems.tsx
@@ -3,6 +3,7 @@ import {
   Button,
   Divider,
   IconButton,
+  Menu,
   MenuItem,
   Stack,
   styled,
@@ -11,11 +12,16 @@ import {
 } from '@mui/material'
 import SearchIcon from '@mui/icons-material/Search'
 import avatar from '../../assets/avatar.svg'
+import notifications from '../../assets/notifications.svg'
+import logout from '../../assets/logout.svg'
 
 import styles from './Header.module.css'
 import { useState } from 'react'
 import menuIcon from '../../assets/menu.svg'
 import { SideBar } from './SideBar'
+import { useUserContext } from '../../context/UserContext'
+import { googleLogout } from '@react-oauth/google'
+import { useNavigate } from 'react-router-dom'
 
 const StyledTextField = styled(TextField)(({ theme }) => ({
   '& input::placeholder': {
@@ -28,11 +34,34 @@ const StyledTextField = styled(TextField)(({ theme }) => ({
 
 export const MenuItems = () => {
   const [open, setOpen] = useState(false)
+  const { profile, setProfile, setUser } = useUserContext()
+  const navigate = useNavigate()
 
   const toggleDrawer = (newOpen: boolean) => () => {
     setOpen(newOpen)
   }
 
+  const navigateToLoginPage = () => {
+    navigate('/login')
+  }
+
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null)
+  const openMenu = Boolean(anchorEl)
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget)
+  }
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const logOut = () => {
+    googleLogout()
+    setUser(undefined)
+    setProfile(undefined)
+    navigateToLoginPage()
+  }
+
+  console.log(profile)
   return (
     <>
       <Stack
@@ -52,9 +81,69 @@ export const MenuItems = () => {
           <Typography variant='h6'>Help</Typography>
         </MenuItem>
         <div className={styles.notificationWrapper}>
-          <IconButton className={styles.logoIcon}>
-            <Avatar src={avatar} />
-          </IconButton>
+          {profile ? (
+            <>
+              <IconButton className={styles.logoIcon} onClick={handleClick}>
+                <Avatar src={profile.picture} />
+              </IconButton>
+              <Menu
+                anchorEl={anchorEl}
+                open={openMenu}
+                onClose={handleClose}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'center',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'center',
+                }}
+              >
+                <div className={styles.dropDownMenu}>
+                  <MenuItem onClick={handleClose}>
+                    <Typography variant='h6' className={styles.userName}>
+                      {profile.name}
+                    </Typography>
+                  </MenuItem>
+                  <MenuItem onClick={handleClose}>
+                    <Typography variant='body2' className={styles.userEmail}>
+                      {profile.email}
+                    </Typography>
+                  </MenuItem>
+                  <div className={styles.dropDownNotificationMenu}>
+                    <div className={styles.notificationsIcon}>
+                      <MenuItem onClick={handleClose}>
+                        <div className={styles.notificationMenuBadge} />
+                        <img src={notifications} />
+                        <Typography
+                          variant='body1'
+                          className={styles.menuNotification}
+                        >
+                          Notifications
+                        </Typography>
+                      </MenuItem>
+                    </div>
+                    <Divider />
+                    <div className={styles.signOutIcon}>
+                      <MenuItem onClick={logOut}>
+                        <img src={logout} />
+                        <Typography
+                          variant='body1'
+                          className={styles.menuNotification}
+                        >
+                          Sign out
+                        </Typography>
+                      </MenuItem>
+                    </div>
+                  </div>
+                </div>
+              </Menu>
+            </>
+          ) : (
+            <IconButton className={styles.logoIcon} onClick={handleClick}>
+              <Avatar src={avatar} />
+            </IconButton>
+          )}
           <div className={styles.notificationBadge} />
         </div>
 
